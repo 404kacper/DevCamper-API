dd3d3e9f8d29e225a32be62ea0b9cf50cf062083
Kacper <404kacper>
2020-05-08 09:38:18 +0200

    Added routes, controllers for req/res and custom logger middleware

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
+// const logger = require('./middleware/logger');
+
+// Route files
+const bootcamps = require('./routes/bootcamps');
 
 // Load env vars
 dotenv.config({ path: './config/config.env' });
 
 const app = express();
+
+// Simple logger middleware
+// app.use(logger);
+
+// Dev logging middleware
+if(process.env.NODE_ENV == 'development') {
+    app.use(morgan('dev'))
+}
+
+// Mount routers
+app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
